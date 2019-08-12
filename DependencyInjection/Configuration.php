e1cde6637697e0c45f84317638be77bfeaf05bf8
Jonas Hauser <hauser@hasch.io>
2019-08-12 12:30:16 +0200

    Updated TreeBuilder to support Symfony 5.0

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -26,8 +26,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('notification');
+        $treeBuilder = new TreeBuilder('notification');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
